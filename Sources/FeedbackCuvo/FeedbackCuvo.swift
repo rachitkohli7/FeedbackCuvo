2e158b3bdfd80eb3dc4cfd89f6fb327e4051116e
rachit kohli <rachit.kohli@pinimbus.com>
2022-12-29 18:05:04 +0530

    initial commit

--- Sources/FeedbackCuvo/FeedbackCuvo.swift
+++ Sources/FeedbackCuvo/FeedbackCuvo.swift
@@ -1,6 +1,45 @@
+import SwiftUI
+
 public struct FeedbackCuvo {
-    public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    @State private var rating = 4;
+    @State private var feedbackString = "";
+    @State private var email = "";
+    @State private var isExpanded = false;
+    @State private var chosenCategory = "";
+    var categories: [String] = ["Display", "Performance", "Other"]
+    var body: some View {
+        NavigationView{
+            Form{
+                Section(header: Text("Choose rating")){
+                    HStack(alignment: .center){
+                        RatingView(rating: $rating, label: "Feedback")
+                    }
+                }
+                Section(header: Text("Feedback")) {
+                    TextField("Your Email Address", text: $email)
+                    DisclosureGroup(chosenCategory != "" ? chosenCategory : "Choose Category", isExpanded: $isExpanded) {
+                        ForEach(categories, id: \.self) {category in
+                            Text("\(category)")
+                                .onTapGesture {
+                                    self.chosenCategory = category;
+                                    withAnimation{
+                                        self.isExpanded.toggle();
+                                    }
+                                }
+                            }
+                    }
+                    
+                }
+                Section(header: Text("Write your feedback here")) {
+                    TextEditor(text: $feedbackString).lineLimit(3)
+                        .frame(maxHeight: 200)
+                }
+            }
+            .navigationTitle("CuVo - Customer Voice")
+        }
     }
 }
