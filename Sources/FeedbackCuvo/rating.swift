785ce2b5a81bb2020532535ffc3291d95f477c4e
rachit kohli <rachitkohli7@gmail.com>
2022-12-29 18:18:13 +0530

    Adding rating.swift

---
+++ Sources/FeedbackCuvo/rating.swift
@@ -0,0 +1,51 @@
+//
+//  rating.swift
+//  feedbackApp
+//
+//  Created by rachit kohli on 27/12/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct RatingView: View {
+    
+    @Binding var rating: Int
+
+    var label = ""
+
+    var maximumRating = 5
+
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+    
+    var body: some View {
+//        if label.isEmpty == false {
+//            Text(label)
+//        }
+        HStack {
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+                    .onTapGesture {
+                        rating = number
+                    }
+            }
+        }
+    }
+    
+    
+}
+
+
